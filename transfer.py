89ae0a148ed8badde22a87a1e51918bdb1dc6713
Ubuntu <ubuntu@ip-172-31-7-29.eu-central-1.compute.internal>
2017-08-05 07:27:25 +0000

    updated transfer model

--- transfer.py
+++ transfer.py
@@ -10,7 +10,7 @@ img_dir = './data/img'
 meta_dir = './data/meta'
 train_dir = './data/train'
 test_dir = './data/test'
-top_model_weights_path = './bottleneck_fc_model.h5'
+top_model_weights_path = './weights/bottleneck_fc_model.h5'
 
 # Get metadata (removes duplicates)
 def get_gender_metadata(file):
@@ -35,10 +35,10 @@ copy_files(male_test, os.path.join(test_dir, 'male'))
 
 # dimensions of our images.
 img_width, img_height = 150, 150
-nb_train_samples = len(train_all)
-nb_test_samples = len(test_all)
 epochs = 50
 batch_size = 16
+nb_train_samples = len(train_all)
+nb_test_samples = len(test_all)
 
 def save_bottlebeck_features():
     datagen = ImageDataGenerator(rescale=1. / 255)
@@ -49,34 +49,31 @@ def save_bottlebeck_features():
     generator = datagen.flow_from_directory(
         train_dir,
         target_size=(img_width, img_height),
-        batch_size=batch_size,
+        batch_size=1,
         class_mode=None,
         shuffle=False)
-    bottleneck_features_train = model.predict_generator(
-        generator, nb_train_samples // batch_size)
-    np.save(open('bottleneck_features_train.npy', 'w'),
-            bottleneck_features_train)
+    bottleneck_features_train = model.predict_generator(generator, nb_train_samples)
+    bottleneck_labels_train = np.array([0]*len(female_train) + [1]*len(male_train))
+    np.save(open('bottleneck_features_train.npy', 'w'), bottleneck_features_train)
+    np.save(open('bottleneck_labels_train.npy', 'w'), bottleneck_labels_train)
 
     generator = datagen.flow_from_directory(
         test_dir,
         target_size=(img_width, img_height),
-        batch_size=batch_size,
+        batch_size=1,
         class_mode=None,
         shuffle=False)
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'w'),
-            bottleneck_features_validation)
-
+    bottleneck_features_test = model.predict_generator(generator, nb_test_samples)
+    bottleneck_labels_test = np.array([0]*len(female_test) + [1]*len(male_test))
+    np.save(open('bottleneck_features_test.npy', 'w'), bottleneck_features_test)
+    np.save(open('bottleneck_labels_test.npy', 'w'), bottleneck_labels_test)
 
 def train_top_model():
     train_data = np.load(open('bottleneck_features_train.npy'))
-    train_labels = np.array(
-        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
+    train_labels = np.load(open('bottleneck_labels_train.npy'))
 
-    validation_data = np.load(open('bottleneck_features_validation.npy'))
-    validation_labels = np.array(
-        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+    test_data = np.load(open('bottleneck_features_test.npy'))
+    test_labels = np.load(open('bottleneck_labels_test.npy'))
 
     model = Sequential()
     model.add(Flatten(input_shape=train_data.shape[1:]))
@@ -84,15 +81,20 @@ def train_top_model():
     model.add(Dropout(0.5))
     model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(optimizer='rmsprop',
-                  loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # class weights to balance training
+    class_weight = {
+        0 : 1.,
+        1: float(len(female_train))/float(len(male_train))
+    }
 
     model.fit(train_data, train_labels,
               epochs=epochs,
               batch_size=batch_size,
-              validation_data=(validation_data, validation_labels))
+              validation_data=(test_data, test_labels),
+              class_weight=class_weight)
     model.save_weights(top_model_weights_path)
-
 
 save_bottlebeck_features()
 train_top_model()
