5977f07005ccbb179b2271dfadf13daa8f302588
Ethan Brown <ewb@ewb.io>
2017-08-06 11:14:43 +0200

    train

--- train_naive.py
+++ train_naive.py
@@ -16,6 +16,5 @@ class_weight = {
 
 # Train
 model = NaiveModel(tmp_dir, 'naive', img_width, img_height)
-model.load(os.path.join(results_dir, 'naive.h5'))
-print model.evaluate(test_dir, batch_size)
-print model.predict(test_dir, batch_size)
+model.train(train_dir, test_dir, epochs, batch_size, class_weight)
+model.save(os.path.join(results_dir, 'naive.h5'))
